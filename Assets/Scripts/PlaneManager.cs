20fff7f48b3837313504a23ccd93e195e52b3439
Jigar Bhanushali <www.jnb.com@gmail.com>
2026-06-23 12:22:28 +0530

    v2 removed boarder and made single plane

--- Assets/Scripts/PlaneManager.cs
+++ Assets/Scripts/PlaneManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using UnityEngine.XR.ARFoundation;
 
 public class PlaneManager : MonoBehaviour
 {
@@ -17,14 +17,43 @@ public class PlaneManager : MonoBehaviour
     [SerializeField]
     private Texture[] _textures;
 
+    private ARPlaneManager _arPlaneManager;
+    private ARPlane _firstPlane;
 
     // Awake is called as soon as the app starts.
     void Awake()
     {
-
+        _arPlaneManager = FindObjectOfType<ARPlaneManager>();
+        if (_arPlaneManager != null) {
+            _arPlaneManager.planesChanged += OnPlanesChanged;
+        }
 
         for(int i=0; i < 5; i++) {
             addImageListeners(i);
+        }
+    }
+
+    private void OnPlanesChanged(ARPlanesChangedEventArgs args)
+    {
+        if (_firstPlane == null && args.added.Count > 0)
+        {
+            _firstPlane = args.added[0];
+        }
+
+        foreach (var plane in args.added)
+        {
+            if (plane != _firstPlane)
+            {
+                plane.gameObject.SetActive(false);
+            }
+        }
+
+        foreach (var plane in args.updated)
+        {
+            if (plane != _firstPlane && plane.gameObject.activeSelf)
+            {
+                plane.gameObject.SetActive(false);
+            }
         }
     }
 
